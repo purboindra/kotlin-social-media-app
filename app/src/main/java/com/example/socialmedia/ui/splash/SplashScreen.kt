47120922a8756c12850431837cac6d22fa044522
purboyndra <purboindra@gmail.com>
2025-01-16 06:48:47 +0700

    fix: fontweight and logo size SplashScreen

--- app/src/main/java/com/example/socialmedia/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/example/socialmedia/ui/splash/SplashScreen.kt
@@ -8,21 +8,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
-import coil3.request.ImageRequest
-import coil3.svg.SvgDecoder
 import com.example.socialmedia.R
 import com.example.socialmedia.ui.components.SvgImage
-import java.io.File
 
 @Composable
 fun SplashScreen() {
@@ -37,12 +32,14 @@ fun SplashScreen() {
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text("Social Media App")
+                Text("Instagram", style = MaterialTheme.typography.headlineLarge.copy(
+                    fontWeight = FontWeight.Bold,
+                ))
                 Spacer(modifier = Modifier.height(16.dp))
                 SvgImage(
                     R.raw.instagram_logo,
                     contentDescription = "Instagram Logo",
-                    modifier = Modifier.size(64.dp)
+                    modifier = Modifier.size(102.dp)
                 )
             }
         }
