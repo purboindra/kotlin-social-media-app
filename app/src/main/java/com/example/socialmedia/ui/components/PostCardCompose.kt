54c5207f86268fac260c5d0c27f9fd28a201076e
purboyndra <purboindra@gmail.com>
2025-02-15 05:51:34 +0700

    refactor(PostCardCompose): Use Image instead Icon for Message

--- app/src/main/java/com/example/socialmedia/ui/components/PostCardCompose.kt
+++ app/src/main/java/com/example/socialmedia/ui/components/PostCardCompose.kt
@@ -1,10 +1,8 @@
 package com.example.socialmedia.ui.components
 
-import android.content.Context
-import android.provider.CalendarContract.Colors
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,53 +13,24 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.outlined.Message
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.outlined.FavoriteBorder
-import androidx.compose.material.icons.outlined.Save
 import androidx.compose.material.icons.outlined.Share
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.produceState
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil3.compose.AsyncImage
-import coil3.request.ImageRequest
-import coil3.request.crossfade
-import com.example.socialmedia.R
 import com.example.socialmedia.data.model.PostModel
-import com.example.socialmedia.ui.theme.BlueLight
-import com.example.socialmedia.ui.theme.GrayDark
-import com.example.socialmedia.ui.theme.GrayPrimary
+import com.example.socialmedia.icons.Message
+import com.example.socialmedia.icons.message.Message
 import com.example.socialmedia.ui.viewmodel.PostViewModel
 import com.example.socialmedia.utils.HorizontalSpacer
 import com.example.socialmedia.utils.VerticalSpacer
-import com.example.socialmedia.utils.imageLoader
 
 @Composable
 fun PostCardCompose(
@@ -123,16 +92,8 @@ fun PostCardCompose(
                         postModel = postModel
                     )
                     5.HorizontalSpacer()
-                    IconButton(
-                        onClick = {},
-                        modifier = Modifier.size(24.dp)
+                    Image(imageVector = Message.Message, contentDescription = "Message", modifier = Modifier.size(24.dp))
                     
-                    ) {
-                        Icon(
-                            Icons.AutoMirrored.Outlined.Message,
-                            contentDescription = "Message",
-                        )
-                    }
                     5.HorizontalSpacer()
                     IconButton(
                         onClick = {},
@@ -180,8 +141,7 @@ fun PostCardCompose(
                 postViewModel,
                 id = postModel.id,
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = horizontalPadding)
+                    .fillMaxWidth(),
             )
         }
     }
