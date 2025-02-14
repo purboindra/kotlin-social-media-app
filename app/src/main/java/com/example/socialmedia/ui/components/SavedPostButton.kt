77fbf63461e69cc973191586a8b0bb2654df548c
purboyndra <purboindra@gmail.com>
2025-02-15 05:51:59 +0700

    feat(SavedPostButton): invoke saved post

--- app/src/main/java/com/example/socialmedia/ui/components/SavedPostButton.kt
+++ app/src/main/java/com/example/socialmedia/ui/components/SavedPostButton.kt
@@ -1,16 +1,15 @@
 package com.example.socialmedia.ui.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Save
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.unit.dp
+import com.example.socialmedia.icons.Save
+import com.example.socialmedia.icons.save.Save
 import com.example.socialmedia.ui.viewmodel.PostViewModel
 
 @Composable
@@ -20,16 +19,15 @@ fun SavedPostButton(
     postViewModel: PostViewModel
 ) {
     
-    val savedPostState by postViewModel.savedPostState.collectAsState()
-    
-    Icon(
-        Icons.Outlined.Save,
+    Image(
+        imageVector = Save.Save,
         contentDescription = "Save",
-        tint = if(hasSaved) Color.Yellow else Color.Gray,
         modifier = Modifier
             .size(24.dp)
             .clickable {
-                postViewModel.savedPost(id)
-            }
+                postViewModel.invokeSavedPost(id)
+            },
+        colorFilter = ColorFilter.tint(if (hasSaved) Color.Red else Color.Gray)
     )
+
 }
