bb5944a7dd0b3bcf8b089c335a92545a45703555
purboyndra <purboindra@gmail.com>
2025-03-09 13:26:21 +0700

    fix: remove unnecessary import InstaStoryScreen

--- app/src/main/java/com/example/socialmedia/ui/insta_story/InstaStoryScreen.kt
+++ app/src/main/java/com/example/socialmedia/ui/insta_story/InstaStoryScreen.kt
@@ -3,8 +3,6 @@ package com.example.socialmedia.ui.insta_story
 import android.Manifest
 import android.net.Uri
 import android.os.Build
-import android.util.Log
-import android.view.MotionEvent
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -12,36 +10,23 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Cameraswitch
-import androidx.compose.material.icons.outlined.Close
-import androidx.compose.material.icons.outlined.FlashOn
-import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.RadioButton
@@ -58,39 +43,27 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.navigation.NavHostController
 import coil3.compose.rememberAsyncImagePainter
 import com.example.socialmedia.ui.components.AppElevatedButton
-import com.example.socialmedia.ui.components.InstaStoryContent
 import com.example.socialmedia.ui.theme.BlueLight
 import com.example.socialmedia.ui.theme.BluePrimary
 import com.example.socialmedia.ui.theme.GrayPrimary
-import com.example.socialmedia.ui.viewmodel.CameraViewModel
 import com.example.socialmedia.ui.viewmodel.InstastoryViewModel
 import com.example.socialmedia.utils.PermissionHelper
 import com.example.socialmedia.utils.PostHelper
-import com.example.socialmedia.utils.VerticalSpacer
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InstaStoryScreen(
     navHostController: NavHostController,
-    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
-    cameraViewModel: CameraViewModel = hiltViewModel(),
     instaStoryViewModel: InstastoryViewModel = hiltViewModel(),
 ) {
     val context = LocalContext.current
