252debe67bc7ac020e2e432009540a169000c137
purboyndra <purboindra@gmail.com>
2025-03-11 04:35:44 +0700

    fix: Implement LikeScreen

--- app/src/main/java/com/example/socialmedia/ui/like/LikeScreen.kt
+++ app/src/main/java/com/example/socialmedia/ui/like/LikeScreen.kt
@@ -1,10 +1,129 @@
 package com.example.socialmedia.ui.like
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import coil3.compose.AsyncImage
+import com.example.socialmedia.data.model.State
+import com.example.socialmedia.ui.viewmodel.PostViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LikeScreen(navHostController: NavHostController) {
-    Text("Hello Like Screen")
+fun LikeScreen(
+    postViewModel: PostViewModel = hiltViewModel<PostViewModel>(),
+    navHostController: NavHostController
+) {
+    
+    val likesState by postViewModel.allLikesState.collectAsState()
+    
+    LaunchedEffect(Unit) {
+        postViewModel.fetchAllLikes()
+    }
+    
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = { navHostController.popBackStack() }) {
+                        Icon(
+                            Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                },
+                title = {
+                    Text(
+                        "Likes",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier
+                            .fillMaxWidth(
+                                0.87f
+                            ),
+                        textAlign = TextAlign.Center,
+                    )
+                }
+            )
+        }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .padding(paddingValues)
+                .padding(horizontal = 16.dp),
+        ) {
+            when (likesState) {
+                is State.Success -> {
+                    val items = (likesState as State.Success).data
+                    LazyVerticalStaggeredGrid(
+                        columns = StaggeredGridCells.Fixed(3),
+                        verticalItemSpacing = 4.dp,
+                        horizontalArrangement = Arrangement.spacedBy(4.dp),
+                        content = {
+                            items(items) { like ->
+                                AsyncImage(
+                                    model = like.post.imageUrl,
+                                    contentScale = ContentScale.Crop,
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .wrapContentHeight()
+                                )
+                            }
+                        },
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+                
+                is State.Failure -> {
+                    val errorMessage =
+                        (likesState as State.Failure).throwable.message
+                    Text(
+                        text = errorMessage ?: "An error occurred",
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+                
+                is State.Loading -> {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        CircularProgressIndicator()
+                    }
+                }
+                
+                else -> {
+                
+                }
+            }
+        }
+    }
 }
