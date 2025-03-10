28a80aac90e85b8006e7ab476960a13d43883c0b
purboyndra <purboindra@gmail.com>
2025-03-11 06:11:31 +0700

    feat: ProfileHeaderCompose

---
+++ app/src/main/java/com/example/socialmedia/ui/components/ProfileHeaderCompose.kt
@@ -0,0 +1,85 @@
+package com.example.socialmedia.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import com.example.socialmedia.utils.HorizontalSpacer
+import com.example.socialmedia.utils.VerticalSpacer
+
+data class ProfileHeaderComposeParams(
+    val userName: String,
+    val profilePicture: String,
+)
+
+@Composable
+fun ProfileHeaderCompose(
+    params: ProfileHeaderComposeParams
+) {
+    Row(
+        modifier = Modifier
+            .height(104.dp)
+            .fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        AsyncImage(
+            model = params.profilePicture,
+            contentDescription = params.userName,
+            modifier = Modifier
+                .size(64.dp)
+                .clip(RoundedCornerShape(100)),
+            contentScale = ContentScale.Crop
+        )
+        15.HorizontalSpacer()
+        Row(
+            horizontalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            ProfileStatus(
+                "Posts",
+                value = "54"
+            )
+            ProfileStatus(
+                "Followers",
+                value = "923"
+            )
+            ProfileStatus(
+                "Following",
+                value = "411"
+            )
+        }
+    }
+}
+
+@Composable
+private fun ProfileStatus(
+    label: String,
+    value: String,
+) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            value, style = MaterialTheme.typography.titleLarge.copy(
+                fontWeight = FontWeight.SemiBold,
+            )
+        )
+        Text(label, style = MaterialTheme.typography.titleSmall)
+    }
+}
--- app/src/main/java/com/example/socialmedia/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/socialmedia/ui/profile/ProfileScreen.kt
@@ -1,8 +1,23 @@
 package com.example.socialmedia.ui.profile
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreHoriz
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -10,22 +25,29 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.socialmedia.data.model.State
 import com.example.socialmedia.ui.components.AppElevatedButton
+import com.example.socialmedia.ui.components.ProfileHeaderCompose
+import com.example.socialmedia.ui.components.ProfileHeaderComposeParams
 import com.example.socialmedia.ui.navigation.Screens
 import com.example.socialmedia.ui.viewmodel.AuthViewModel
+import com.example.socialmedia.ui.viewmodel.ProfileViewModel
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(
     navHostController: NavHostController,
-    authViewModel: AuthViewModel = hiltViewModel()
+    authViewModel: AuthViewModel = hiltViewModel(),
+    profileViewModel: ProfileViewModel = hiltViewModel(),
 ) {
     
     val logoutState by authViewModel.logoutState.collectAsState()
-    val coroutineScope = rememberCoroutineScope()
+    val profilePicture by profileViewModel.profilePicture.collectAsState()
+    val userName by profileViewModel.username.collectAsState()
     
     LaunchedEffect(logoutState) {
         if (logoutState is State.Success) {
@@ -33,18 +55,44 @@ fun ProfileScreen(
         }
     }
     
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        AppElevatedButton(
-            onClick = {
-                coroutineScope.launch {
-                    authViewModel.logout()
+    LaunchedEffect(Unit) {
+        profileViewModel.getProfilePicture()
+        profileViewModel.getUserName()
+    }
+    
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Profile")
+                },
+                actions = {
+                    IconButton(
+                        onClick = {}
+                    ) {
+                        Icon(
+                            Icons.Default.MoreHoriz,
+                            contentDescription = "More",
+                            modifier = Modifier.size(24.dp)
+                        )
+                    }
                 }
-            },
-            text = "Logout"
-        )
+            )
+        }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(paddingValues)
+                .padding(horizontal = 16.dp)
+        ) {
+            item {
+                ProfileHeaderCompose(
+                    ProfileHeaderComposeParams(
+                        userName = userName ?: "",
+                        profilePicture = profilePicture ?: "",
+                    )
+                )
+            }
+        }
     }
 }
