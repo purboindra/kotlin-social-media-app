178b7a2b4d4cc1880e75abeda19611d1de3ce0a7
purboyndra <purboindra@gmail.com>
2025-03-12 06:40:37 +0700

    feat: implement SearchScreen

--- app/src/main/java/com/example/socialmedia/ui/search/SearchScreen.kt
+++ app/src/main/java/com/example/socialmedia/ui/search/SearchScreen.kt
@@ -15,22 +15,37 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.example.socialmedia.data.model.State
 import com.example.socialmedia.ui.components.AppOutlinedTextField
+import com.example.socialmedia.ui.viewmodel.SearchViewModel
 import com.example.socialmedia.utils.VerticalSpacer
 
 @Composable
-fun SearchScreen(navHostController: NavHostController) {
+fun SearchScreen(
+    navHostController: NavHostController,
+    searchViewModel: SearchViewModel = hiltViewModel()
+) {
+    
+    val searchState by searchViewModel.searchState.collectAsState()
+    val query by searchViewModel.queryState.collectAsState()
+    
     Scaffold { paddingValues ->
         Column(modifier = Modifier.padding(paddingValues)) {
             /// SEARCH
             AppOutlinedTextField(
-                query = "",
-                onValueChange = {},
+                query = query,
+                onValueChange = {
+                    searchViewModel.onChangeQuery(it)
+                },
                 placeholderText = "Search...",
                 trailingIcon = {
                     Icon(
@@ -46,20 +61,30 @@ fun SearchScreen(navHostController: NavHostController) {
                     )
             )
             5.VerticalSpacer()
+            when (searchState) {
+                is State.Loading -> Text("Loading...")
+                is State.Success -> Text("Results: ${(searchState as State.Success).data}")
+                is State.Failure -> Text("Error: ${(searchState as State.Failure).throwable.message}")
+                State.Idle -> Text("Type something to search...")
+            }
             /// CONTENT
             LazyVerticalStaggeredGrid(
                 columns = StaggeredGridCells.Fixed(3),
                 verticalItemSpacing = 4.dp,
                 horizontalArrangement = Arrangement.spacedBy(4.dp),
-                modifier = Modifier.fillMaxSize().weight(1f),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f),
             ) {
                 items(10) {
-                    Box(modifier = Modifier
-                        .fillMaxWidth()
-                        .height((100..300).random().dp)
-                        .background(Color.LightGray))
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height((100..300).random().dp)
+                            .background(Color.LightGray)
+                    )
                 }
-
+                
             }
         }
     }
