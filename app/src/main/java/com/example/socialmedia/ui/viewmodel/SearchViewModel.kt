b951fa42e7321ad710f75bde03313a567d6b02f3
purboyndra <purboindra@gmail.com>
2025-03-12 06:39:30 +0700

    feat: SearchViewModel

---
+++ app/src/main/java/com/example/socialmedia/ui/viewmodel/SearchViewModel.kt
@@ -0,0 +1,56 @@
+package com.example.socialmedia.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.socialmedia.data.model.State
+import com.example.socialmedia.data.model.UserModel
+import com.example.socialmedia.domain.usecases.UserUsecase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+    private val userUsecase: UserUsecase
+) : ViewModel() {
+    
+    private val _searchState =
+        MutableStateFlow<State<List<UserModel>>>(State.Idle)
+    val searchState = _searchState.asStateFlow()
+    
+    private val _queryState = MutableStateFlow("")
+    val queryState = _queryState.asStateFlow()
+    
+    init {
+        observeQueryChange()
+    }
+    
+    @OptIn(FlowPreview::class)
+    private fun observeQueryChange() {
+        viewModelScope.launch {
+            _queryState.debounce(
+                500
+            ).distinctUntilChanged().collectLatest { query ->
+                if (query.isNotBlank()) {
+                    _searchState.value = State.Loading
+                    userUsecase.search(query)
+                        .collectLatest { state ->
+                            _searchState.value = state
+                        }
+                } else {
+                    _searchState.value = State.Idle
+                }
+            }
+        }
+    }
+    
+    fun onChangeQuery(query: String) {
+        _queryState.value = query
+    }
+}
