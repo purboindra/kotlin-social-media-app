75fdae9052bbf6a031834008d0bf73767b362119
purboindra <purboindra@gmail.com>
2025-03-12 16:14:37 +0700

    feat: getUserId MainViewModel

--- app/src/main/java/com/example/socialmedia/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/socialmedia/ui/viewmodel/MainViewModel.kt
@@ -1,24 +1,36 @@
 package com.example.socialmedia.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.socialmedia.data.db.local.AppDataStore
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val dataStore: AppDataStore
 ) : ViewModel() {
-    
+
     private val _username = MutableStateFlow("")
     val username = _username.asStateFlow()
-    
+
+    private val _userId = MutableStateFlow("")
+    val userId = _userId.asStateFlow()
+
     private val _bottomNavbarIndex = MutableStateFlow(0)
     val bottomNavbarIndex = _bottomNavbarIndex.asStateFlow()
-    
+
     fun onSelectedBottomNavbar(index: Int) {
         _bottomNavbarIndex.value = index
+    }
+
+    fun getUserId() = viewModelScope.launch {
+        dataStore.userId.collectLatest { state ->
+            _userId.value = state ?: ""
+        }
     }
 }
