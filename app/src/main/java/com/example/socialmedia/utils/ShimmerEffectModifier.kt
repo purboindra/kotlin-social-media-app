5766701074cc53c7959318d8106a81612701a313
purboindra <purboindra@gmail.com>
2025-03-12 09:53:18 +0700

    feat: ShimmerEffectModifier.kt

---
+++ app/src/main/java/com/example/socialmedia/utils/ShimmerEffectModifier.kt
@@ -0,0 +1,49 @@
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun Modifier.shimmerLoading(
+    durationMillis: Int = 1000,
+): Modifier {
+    val shimmerColors = listOf(
+        Color.LightGray.copy(alpha = 0.6f),
+        Color.LightGray.copy(alpha = 0.3f),
+        Color.LightGray.copy(alpha = 0.6f)
+    )
+
+    val transition = rememberInfiniteTransition(label = "")
+
+    val translateAnimation by transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1000f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = durationMillis,
+                easing = LinearEasing,
+            ),
+            repeatMode = RepeatMode.Restart,
+        ),
+        label = "",
+    )
+
+    return this.drawBehind {
+        val brush = Brush.linearGradient(
+            colors = shimmerColors,
+            start = Offset(translateAnimation, 0f),
+            end = Offset(translateAnimation + 300f, 0f),
+        )
+
+        drawRect(brush = brush)
+    }
+}
