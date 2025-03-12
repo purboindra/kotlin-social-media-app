3fd5ee55b6fee1770583e094047fcfc168349a14
purboyndra <purboindra@gmail.com>
2025-03-13 05:55:41 +0700

    feat: Implement fetch user by id ProfileScreen

--- app/src/main/java/com/example/socialmedia/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/socialmedia/ui/profile/ProfileScreen.kt
@@ -1,10 +1,13 @@
 package com.example.socialmedia.ui.profile
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreHoriz
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -15,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -32,12 +36,11 @@ fun ProfileScreen(
     navHostController: NavHostController,
     authViewModel: AuthViewModel = hiltViewModel(),
     profileViewModel: ProfileViewModel = hiltViewModel(),
-    userId:String,
+    userId: String,
 ) {
     
     val logoutState by authViewModel.logoutState.collectAsState()
-    val profilePicture by profileViewModel.profilePicture.collectAsState()
-    val userName by profileViewModel.username.collectAsState()
+    val userState by profileViewModel.userState.collectAsState()
     
     LaunchedEffect(logoutState) {
         if (logoutState is State.Success) {
@@ -46,46 +49,77 @@ fun ProfileScreen(
     }
     
     LaunchedEffect(Unit) {
-        profileViewModel.getProfilePicture()
-        profileViewModel.getUserName()
+        profileViewModel.fetchUserById(userId)
     }
     
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(userName ?: "Profile")
-                },
-                actions = {
-                    IconButton(
-                        onClick = {}
-                    ) {
-                        Icon(
-                            Icons.Default.MoreHoriz,
-                            contentDescription = "More",
-                            modifier = Modifier.size(24.dp)
-                        )
-                    }
-                }
-            )
-        }
-    ) { paddingValues ->
-        LazyColumn(
-            modifier = Modifier
-                .padding(paddingValues)
-                .padding(horizontal = 16.dp)
-        ) {
-            item {
-                ProfileHeaderCompose(
-                    ProfileHeaderComposeParams(
-                        userName = userName ?: "",
-                        profilePicture = profilePicture ?: "",
+    when (userState) {
+        is State.Success -> {
+            val user = (userState as State.Success).data
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(user.username ?: "Profile")
+                        },
+                        actions = {
+                            IconButton(
+                                onClick = {}
+                            ) {
+                                Icon(
+                                    Icons.Default.MoreHoriz,
+                                    contentDescription = "More",
+                                    modifier = Modifier.size(24.dp)
+                                )
+                            }
+                        }
                     )
-                )
+                }
+            ) { paddingValues ->
+                LazyColumn(
+                    modifier = Modifier
+                        .padding(paddingValues)
+                        .padding(horizontal = 16.dp)
+                ) {
+                    item {
+                        ProfileHeaderCompose(
+                            ProfileHeaderComposeParams(
+                                userName = user.fullName ?: "",
+                                profilePicture = user.profilePicture ?: "",
+                            )
+                        )
 //            ProfileCaptionCompose(
 //                bio =
 //            )
-                
+                    
+                    }
+                }
+            }
+        }
+        
+        is State.Failure -> {
+            val message = (userState as State.Failure).throwable.message
+            Scaffold { paddingValues ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(message ?: "Unknown Error Occurred")
+                }
+            }
+        }
+        
+        else -> {
+            Scaffold { paddingValues ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
             }
         }
     }
