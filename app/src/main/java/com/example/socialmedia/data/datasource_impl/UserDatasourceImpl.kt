58b120451974a415c457a090d59e87587755b79a
purboindra <purboindra@gmail.com>
2025-03-14 14:06:07 +0700

    fix: add @Serializable data class Follow

--- app/src/main/java/com/example/socialmedia/data/datasource_impl/UserDatasourceImpl.kt
+++ app/src/main/java/com/example/socialmedia/data/datasource_impl/UserDatasourceImpl.kt
@@ -13,8 +13,10 @@ import io.github.jan.supabase.postgrest.query.Columns
 import io.github.jan.supabase.postgrest.query.filter.TextSearchType
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 
+@Serializable
 data class Follow(
     @SerialName("id")
     val id: String,
@@ -32,9 +34,9 @@ class UserDatasourceImpl(
 ) : UserDatasource {
     override suspend fun fetchAllUsers(query: String?): ResponseModel<List<UserModel>> {
         return try {
-            
+
             val baseQuery = supabase.from("users")
-            
+
             val result = if (query != null) {
                 baseQuery.select {
                     filter {
@@ -44,20 +46,20 @@ class UserDatasourceImpl(
             } else {
                 baseQuery.select()
             }
-            
+
             val data = result.data
-            
+
             val users = Json.decodeFromString<List<UserModel>>(data)
-            
+
             ResponseModel.Success(users)
         } catch (e: Throwable) {
             ResponseModel.Error(e.message ?: "Something went wrong...")
         }
     }
-    
+
     override suspend fun fetchUserById(userId: String): ResponseModel<UserModel> {
         return try {
-            
+
             val result = supabase.from("users").select(
                 columns = Columns.ALL
             ) {
@@ -65,16 +67,16 @@ class UserDatasourceImpl(
                     eq("id", userId)
                 }
             }
-            
+
             val data = result.data
-            
+
             val users = Json.decodeFromString<List<UserModel>>(data)
-            
+
             val user = users.firstOrNull() ?: throw Exception("User not found")
-            
+
             val currentUserId = dataStore.userId.firstOrNull()
                 ?: throw Exception("User ID not found")
-            
+
             val isFollowingResult =
                 supabase.from("follows").select(Columns.ALL) {
                     filter {
@@ -82,58 +84,53 @@ class UserDatasourceImpl(
                         eq("followed_id", userId)
                     }
                 }
-            
+
             val isFollowing =
                 Json.decodeFromString<List<Follow>>(isFollowingResult.data)
-            
-            Log.d(
-                "UserDataSourceImpl",
-                "isFollowing: ${isFollowing}"
-            )
-            
+
             val userFollow = user.copy(
                 isFollow = isFollowing.isNotEmpty()
             )
-            
+
             ResponseModel.Success(userFollow)
-            
+
         } catch (e: Throwable) {
             ResponseModel.Error(e.message ?: "Something went wrong...")
         }
     }
-    
+
     override suspend fun followUser(userId: String): ResponseModel<Boolean> {
         return try {
-            
+
             val currentUserId = dataStore.userId.firstOrNull()
                 ?: throw Exception("User ID not found")
-            
+
             val createFollowModel = CreateFollowModel(
                 followedId = userId,
                 followerId = currentUserId
             )
-            
+
             supabase.from("follows").insert(createFollowModel)
-            
+
             ResponseModel.Success(true)
-            
+
         } catch (e: Throwable) {
             Log.e("UserDataSourceImpl", "Error Follow User: ${e.message}")
             ResponseModel.Error(e.message ?: "Something went wrong...")
         }
     }
-    
+
     override suspend fun unFollowUser(userId: String): ResponseModel<Boolean> {
         return try {
-            
+
             supabase.from("follows").delete {
                 filter {
                     eq("followed_id", userId)
                 }
             }
-            
+
             ResponseModel.Success(true)
-            
+
         } catch (e: Throwable) {
             Log.e("UserDataSourceImpl", "Error Un Follow User: ${e.message}")
             ResponseModel.Error(e.message ?: "Something went wrong...")
