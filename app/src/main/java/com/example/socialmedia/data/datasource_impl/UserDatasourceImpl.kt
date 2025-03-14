3e0c2cdbdb613ff4be0280f3d1359e3edbfe13c3
purboyndra <purboindra@gmail.com>
2025-03-14 20:42:44 +0700

    fix: logic follows UserDatasourceImpl

--- app/src/main/java/com/example/socialmedia/data/datasource_impl/UserDatasourceImpl.kt
+++ app/src/main/java/com/example/socialmedia/data/datasource_impl/UserDatasourceImpl.kt
@@ -19,14 +19,10 @@ import kotlinx.serialization.json.Json
 
 @Serializable
 data class Follow(
-    @SerialName("id")
-    val id: String,
-    @SerialName("created_at")
-    val createdAt: String,
-    @SerialName("follower_id")
-    val followerId: String,
-    @SerialName("followed_id")
-    val followedId: String
+    @SerialName("id") val id: String,
+    @SerialName("created_at") val createdAt: String,
+    @SerialName("follower_id") val followerId: String,
+    @SerialName("followed_id") val followedId: String
 )
 
 class UserDatasourceImpl(
@@ -35,9 +31,9 @@ class UserDatasourceImpl(
 ) : UserDatasource {
     override suspend fun fetchAllUsers(query: String?): ResponseModel<List<UserModel>> {
         return try {
-
+            
             val baseQuery = supabase.from("users")
-
+            
             val result = if (query != null) {
                 baseQuery.select {
                     filter {
@@ -47,20 +43,20 @@ class UserDatasourceImpl(
             } else {
                 baseQuery.select()
             }
-
+            
             val data = result.data
-
+            
             val users = Json.decodeFromString<List<UserModel>>(data)
-
+            
             ResponseModel.Success(users)
         } catch (e: Throwable) {
             ResponseModel.Error(e.message ?: "Something went wrong...")
         }
     }
-
+    
     override suspend fun fetchUserById(userId: String): ResponseModel<UserModel> {
         return try {
-
+            
             val result = supabase.from("users").select(
                 columns = Columns.ALL
             ) {
@@ -68,107 +64,95 @@ class UserDatasourceImpl(
                     eq("id", userId)
                 }
             }
-
+            
             val data = result.data
-
+            
             val users = Json.decodeFromString<List<UserModel>>(data)
-
+            
             val user = users.firstOrNull() ?: throw Exception("User not found")
-
+            
             val currentUserId = dataStore.userId.firstOrNull()
                 ?: throw Exception("User ID not found")
-
-            val isFollowingResult =
-                supabase.from("follows").select(Columns.ALL) {
-                    filter {
-                        eq("follower_id", currentUserId)
-                        eq("followed_id", userId)
-                    }
-                }
-
-            val isFollowing =
-                Json.decodeFromString<List<Follow>>(isFollowingResult.data)
-
-            val userFollow = user.copy(
-                isFollow = isFollowing.isNotEmpty()
-            )
-
-            val followersResult = supabase.from("follows").select(Columns.ALL, {
+            
+            
+            val followersResult = supabase.from("follows").select(Columns.ALL) {
                 filter {
-                    eq("followed_id", currentUserId)
+                    eq("followed_id", userId)
                 }
-            })
-
-            val followers = Json.decodeFromString<List<Follow>>(followersResult.data)
-
-            val followingResult = supabase.from("follows").select(Columns.ALL, {
+            }
+            
+            val followers =
+                Json.decodeFromString<List<Follow>>(followersResult.data)
+            
+            val followingResult = supabase.from("follows").select(Columns.ALL) {
                 filter {
-                    eq("follower_id", currentUserId)
+                    eq("follower_id", userId)
                 }
-            })
-
-            val following = Json.decodeFromString<List<Follow>>(followingResult.data)
-
+            }
+            
+            val following =
+                Json.decodeFromString<List<Follow>>(followingResult.data)
+            
             val followingIds = following.map { it.followedId }
             val followersIds = followers.map { it.followerId }
-
-            val resultFollow = userFollow.copy(
+            
+            val resultFollow = user.copy(
                 followers = followersIds,
-                following = followingIds
+                following = followingIds,
+                isFollow = followersIds.contains(currentUserId)
             )
-
+            
             val postsResult = supabase.from("posts").select(Columns.ALL, {
                 filter {
                     eq("user", currentUserId)
                 }
             })
-
+            
             val post = Json.decodeFromString<List<PostModel>>(postsResult.data)
             val userPostIds = post.map { it.user.id }
-
+            
             val resultPost = resultFollow.copy(
                 posts = userPostIds
             )
-
+            
             ResponseModel.Success(resultPost)
-
+            
         } catch (e: Throwable) {
             ResponseModel.Error(e.message ?: "Something went wrong...")
         }
     }
-
+    
     override suspend fun followUser(userId: String): ResponseModel<Boolean> {
         return try {
-
+            
             val currentUserId = dataStore.userId.firstOrNull()
                 ?: throw Exception("User ID not found")
-
+            
             val createFollowModel = CreateFollowModel(
-                followedId = userId,
-                followerId = currentUserId
+                followedId = userId, followerId = currentUserId
             )
-
+            
             supabase.from("follows").insert(createFollowModel)
-
+            
             ResponseModel.Success(true)
-
+            
         } catch (e: Throwable) {
             Log.e("UserDataSourceImpl", "Error Follow User: ${e.message}")
             ResponseModel.Error(e.message ?: "Something went wrong...")
         }
     }
-
+    
     override suspend fun unFollowUser(userId: String): ResponseModel<Boolean> {
         return try {
-
+            
             supabase.from("follows").delete {
                 filter {
                     eq("followed_id", userId)
                 }
             }
-
+            
             ResponseModel.Success(true)
-
+            
         } catch (e: Throwable) {
             Log.e("UserDataSourceImpl", "Error Un Follow User: ${e.message}")
             ResponseModel.Error(e.message ?: "Something went wrong...")
