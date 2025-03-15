824272e113e45c5d01929b522046e19a49f30478
purboyndra <purboindra@gmail.com>
2025-03-15 14:07:34 +0700

    feat: ProfileTaggedPostsCompose

---
+++ app/src/main/java/com/example/socialmedia/ui/components/ProfileTaggedPostsCompose.kt
@@ -0,0 +1,9 @@
+package com.example.socialmedia.ui.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ProfileTaggedPostsCompose() {
+    Text("ProfileTaggedPostsCompose")
+}
