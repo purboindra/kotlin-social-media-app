8b49e6bf18c6a1e4b4225a47b755ad2175c475e7
purboyndra <purboindra@gmail.com>
2025-03-17 05:12:20 +0700

    feat: FollowScreen

---
+++ app/src/main/java/com/example/socialmedia/ui/follows/FollowScreen.kt
@@ -0,0 +1,17 @@
+package com.example.socialmedia.ui.follows
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+fun FollowScreen(userId: String, navHostController: NavHostController,) {
+    Scaffold { paddingValues ->
+        Column(modifier = Modifier.padding(paddingValues)) {
+        
+        }
+    }
+}
