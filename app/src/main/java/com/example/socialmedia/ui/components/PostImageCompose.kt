0c1b2aa9e35677b774dcbbf0d6e0daa2df023a20
purboyndra <purboindra@gmail.com>
2025-03-16 21:40:42 +0700

    fix: use SubcomposeAsyncImage instead AsyncImage PostImageCompose

--- app/src/main/java/com/example/socialmedia/ui/components/PostImageCompose.kt
+++ app/src/main/java/com/example/socialmedia/ui/components/PostImageCompose.kt
@@ -1,15 +1,16 @@
 package com.example.socialmedia.ui.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import coil3.compose.AsyncImage
+import coil3.compose.SubcomposeAsyncImage
 import coil3.request.ImageRequest
 import coil3.request.crossfade
-import com.example.socialmedia.data.model.PostModel
 import com.example.socialmedia.utils.imageLoader
+import shimmerLoading
 
 @Composable
 fun PostImageCompose(
@@ -18,12 +19,15 @@ fun PostImageCompose(
 ) {
     val context = LocalContext.current
     
-    AsyncImage(
+    SubcomposeAsyncImage(
         model = ImageRequest.Builder(context)
             .data(imageUrl)
             .crossfade(true)
             .build(),
         contentDescription = caption,
+        loading = {
+            Box(modifier = Modifier.shimmerLoading())
+        },
         imageLoader = imageLoader(context),
         modifier = Modifier.fillMaxSize(),
         contentScale = ContentScale.Crop
