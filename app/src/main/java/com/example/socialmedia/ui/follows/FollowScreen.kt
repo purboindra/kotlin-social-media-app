e04e3af5a77779ed41112f0272a2570b3d7a13b4
purboyndra <purboindra@gmail.com>
2025-03-17 18:19:58 +0700

    feat: implement TopAppBar FollowScreen

--- app/src/main/java/com/example/socialmedia/ui/follows/FollowScreen.kt
+++ app/src/main/java/com/example/socialmedia/ui/follows/FollowScreen.kt
@@ -1,18 +1,27 @@
 package com.example.socialmedia.ui.follows
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChevronLeft
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -21,6 +30,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
@@ -32,6 +42,7 @@ import com.example.socialmedia.utils.ConnectionType
 import com.example.socialmedia.utils.HorizontalSpacer
 import com.example.socialmedia.utils.VerticalSpacer
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FollowScreen(
     userId: String,
@@ -41,45 +52,81 @@ fun FollowScreen(
 ) {
     
     val followsState by followsViewModel.followsState.collectAsState()
+    val username by followsViewModel.username.collectAsState()
     
     LaunchedEffect(Unit) {
         followsViewModel.invokeFollows(userId, type)
     }
     
-    Scaffold { paddingValues ->
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        IconButton(
+                            onClick = {}
+                        ) {
+                            Icon(
+                                Icons.Default.ChevronLeft,
+                                contentDescription = "Back"
+                            )
+                        }
+                        Text(
+                            username ?: "",
+                            modifier = Modifier.weight(1f),
+                            textAlign = TextAlign.Center
+                        )
+                        Spacer(modifier = Modifier.width(48.dp))
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
         when (followsState) {
             is State.Success -> {
                 val data = (followsState as State.Success).data
                 Column(modifier = Modifier.padding(paddingValues)) {
                     AppOutlinedTextField(
                         query = "",
-                        onValueChange = {}
+                        onValueChange = {},
+                        placeholderText = "Search user...",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 12.dp)
                     )
                     5.VerticalSpacer()
                     LazyColumn {
-                        items(data){
-                            item ->   Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(12.dp),
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            AsyncImage(
-                                model = item.userFollow.profilePicture,
-                                contentDescription = item.userFollow.username,
-                                modifier = Modifier.clip(
-                                    CircleShape
-                                ),
-                                contentScale = ContentScale.Crop
-                            )
-                            5.HorizontalSpacer()
-                            Column {
-                                Text(
-                                    item.userFollow.username ?: "",
-                                    style = MaterialTheme.typography.labelLarge
+                        items(data) { item ->
+                            
+                            val user =
+                                if (type == ConnectionType.FOLLOWING) item.userFollowed else item.userFollow
+                            
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(12.dp),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                AsyncImage(
+                                    model = user.profilePicture,
+                                    contentDescription = user.username,
+                                    modifier = Modifier.clip(
+                                        CircleShape
+                                    ),
+                                    contentScale = ContentScale.Crop
                                 )
+                                5.HorizontalSpacer()
+                                Column {
+                                    Text(
+                                        user.username ?: "",
+                                        style = MaterialTheme.typography.labelLarge
+                                    )
+                                }
                             }
-                        }
                         }
                     }
                 }
