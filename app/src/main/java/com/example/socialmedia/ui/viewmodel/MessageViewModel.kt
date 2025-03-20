b3df4873147f022a2a4a0f5bdb39cc9f788ce7a9
purboyndra <purboindra@gmail.com>
2025-03-21 05:55:20 +0700

    feat: message state MessageViewModel

--- app/src/main/java/com/example/socialmedia/ui/viewmodel/MessageViewModel.kt
+++ app/src/main/java/com/example/socialmedia/ui/viewmodel/MessageViewModel.kt
@@ -1,15 +1,25 @@
 package com.example.socialmedia.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.socialmedia.data.model.SendMessageModel
+import com.example.socialmedia.data.model.State
 import com.example.socialmedia.domain.usecases.MessageUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class MessageViewModel @Inject constructor(
     private val messageUseCase: MessageUseCase
 ) : ViewModel() {
-
-
-
+    
+    private val _messagesState =
+        MutableStateFlow<State<List<SendMessageModel>>>(State.Idle)
+    val messagesState = _messagesState.asStateFlow()
+    
+    private val _sendMessageState = MutableStateFlow<State<Boolean>>(State.Idle)
+    val sendMessageState = _sendMessageState.asStateFlow()
+    
+    
 }
