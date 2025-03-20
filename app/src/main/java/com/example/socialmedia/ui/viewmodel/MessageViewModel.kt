02396f05f9450fc7100e52f5ab1d58f58bd945b6
purboyndra <purboindra@gmail.com>
2025-03-21 05:56:09 +0700

    feat: fetchMessages MessageViewModel

--- app/src/main/java/com/example/socialmedia/ui/viewmodel/MessageViewModel.kt
+++ app/src/main/java/com/example/socialmedia/ui/viewmodel/MessageViewModel.kt
@@ -1,12 +1,15 @@
 package com.example.socialmedia.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.socialmedia.data.model.SendMessageModel
 import com.example.socialmedia.data.model.State
 import com.example.socialmedia.domain.usecases.MessageUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -21,5 +24,11 @@ class MessageViewModel @Inject constructor(
     private val _sendMessageState = MutableStateFlow<State<Boolean>>(State.Idle)
     val sendMessageState = _sendMessageState.asStateFlow()
     
-    
+    fun fetchMessages() {
+        viewModelScope.launch {
+            messageUseCase.fetchMessages().collectLatest { state ->
+                _messagesState.value = state
+            }
+        }
+    }
 }
