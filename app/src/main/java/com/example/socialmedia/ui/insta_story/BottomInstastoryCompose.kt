f9be7868cbc2b8f7ee817cb4e4624c7fd5d8df88
purboyndra <purboindra@gmail.com>
2025-03-25 22:03:48 +0700

    feat: implement onswitch and ontap BottomInstastoryCompose

--- app/src/main/java/com/example/socialmedia/ui/insta_story/BottomInstastoryCompose.kt
+++ app/src/main/java/com/example/socialmedia/ui/insta_story/BottomInstastoryCompose.kt
@@ -1,35 +1,29 @@
 package com.example.socialmedia.ui.insta_story
 
 import android.net.Uri
-import androidx.camera.core.CameraControl
-import androidx.camera.view.CameraController
-import androidx.camera.view.LifecycleCameraController
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Cameraswitch
 import androidx.compose.material.icons.filled.PhotoCamera
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import coil3.compose.rememberAsyncImagePainter
@@ -41,16 +35,15 @@ import com.example.socialmedia.utils.HorizontalSpacer
 fun BottomInstastoryCompose(
     selectedImage: Uri,
     navHostController: NavHostController,
-    onClick: () -> Unit,
+    onImageTap: () -> Unit,
     onCapture: () -> Unit,
+    onSwitch: () -> Unit,
     image: Uri? = null,
 ) {
     Box(
         modifier = Modifier
             .safeContentPadding()
-            .clickable {
-                onClick()
-            },
+            .padding(vertical = 8.dp, horizontal = 16.dp),
         contentAlignment = Alignment.BottomCenter
     ) {
         Row(verticalAlignment = Alignment.CenterVertically) {
@@ -69,6 +62,9 @@ fun BottomInstastoryCompose(
                     modifier = Modifier
                         .fillMaxSize()
                         .clip(RoundedCornerShape(12.dp))
+                        .clickable {
+                            onImageTap()
+                        }
                 )
             }
             8.HorizontalSpacer()
@@ -106,6 +102,12 @@ fun BottomInstastoryCompose(
                             Icons.Default.Cameraswitch,
                             contentDescription = "Switch Camera",
                             tint = Color.White,
+                            modifier = Modifier
+                                .height(32.dp)
+                                .width(32.dp)
+                                .clickable {
+                                    onSwitch()
+                                }
                         )
                     }
                 } else {
